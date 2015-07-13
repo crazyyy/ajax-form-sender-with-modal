97b802720e7ac0afbbea47ae4bd3f3ce85ff352c
crazyyy <crazyyy@gmail.com>
2015-07-14 01:52:44 +0300

    don

---
+++ html/index.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en" class="no-js">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title> </title>
+
+    <!-- icons -->
+    <link href="favicon.ico" rel="shortcut icon">
+
+    <!-- css + javascript -->
+    <link rel="stylesheet" href="style.css" media="all">
+    <script type="text/javascript" src="///cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
+</head>
+<body>
+<!-- wrapper -->
+<div class="wrapper">
+  <header role="banner">
+    <div class="inner">
+
+      <nav class="nav" role="navigation">
+        <ul class="headnav">
+          <li>menu</li>
+          <li>menu</li>
+        </ul>
+      </nav><!-- /nav -->
+
+    </div><!-- /.inner -->
+  </header><!-- /header -->
+
+  <section role="main">
+    <div class="inner">
+
+      <article>
+
+        <hr>
+
+        <h1 class="ctitle">Пример контента (рыба)</h1>
+      <form class="landform" action="data/form.php" method="POST">
+        <h3>Оставьте заявку</h3>
+        <p><span>на бесплатный тест-драйв</span> в течение 14 дней и наш менеджер перезвонит вам в течение 15 минут</p>
+        <input type="text" name="name" placeholder="Введите Ваше имя" />
+        <input type="tel" name="phone" placeholder="Укажие телефон 0 (000) 000-00-00" />
+        <input type="hidden" name="hidden" value="ok">
+        <button>Отправить</button>
+        <h6 class="message">Отправляем...</h6>
+        <div class="thanx">
+          <h4>Благодарим вас за заявку</h4>
+          <p>Менеджер свяжется с вами в ближайшее время. <br>
+          А пока можете:</p>
+          <ul>
+            <li>посмотреть короткое видео <a  href='https://www.youtube.com/watch?v=9lhxtPUNP4I' target="_blank" class='yt'>«Уничтожитель комаров SketteerVac»</a></li>
+            <li>ознакомиться с <a href='instruction.pdf' target="_blank" class='pdf'>инструкцией по применению</a></li>
+          </ul>
+        </div><!-- thanx -->
+      </form>
+
+
+
+        <hr>
+
+        <h1 class="ctitle">Paragraph</h1>
+
+        <button class="recall">recall</button>
+
+        <hr>
+
+      </article>
+
+
+    </div><!-- /.inner -->
+  </section><!-- /section -->
+</div><!-- /wrapper -->
+
+<footer role="contentinfo">
+  <div class="inner">
+
+    <p class="copyright">
+      © 2015 Собственность. Работает на html2wp easy.
+    </p><!-- /copyright -->
+
+  </div><!-- /.inner -->
+</footer><!-- /footer -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/jquery.js"><\/script>')</script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+</body>
+</html>
